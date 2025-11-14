6c75b47795aecf5468085d9fac6a959da1e051c7
formation <formation@ubuntu.ubuntu-domain>
2025-11-14 11:23:34 +0100

    0.12 - GeometryWithCachedEnvelope

---
+++ src/GeometryWithCachedEnvelope.ts
@@ -0,0 +1,45 @@
+import Geometry from "./Geometry";
+import Enveloppe from "./Enveloppe";
+import GeometryVisitor from "./GeometryVisitor";
+
+export default class GeometryWithCachedEnvelope implements Geometry {
+    private original: Geometry;
+    private cachedEnvelope: Enveloppe | null = null;
+
+    constructor(original: Geometry) {
+        this.original = original;   
+    }
+
+    getEnvelope(): Enveloppe {
+        if (this.cachedEnvelope === null) {
+            this.cachedEnvelope = this.original.getEnvelope();
+        }
+        return this.cachedEnvelope;
+    }
+
+    getType(): string {
+        return this.original.getType();
+    }
+
+    isEmpty(): boolean {
+        return this.original.isEmpty();
+    }
+
+    clone(): Geometry {
+        return new GeometryWithCachedEnvelope(this.original.clone());
+    }
+
+    translate(dx: number, dy: number): void {
+        this.original.translate(dx, dy);
+        this.cachedEnvelope = null; // Invalidate cached envelope
+
+    }
+
+    accept(visitor: GeometryVisitor): void {
+        this.original.accept(visitor);
+    }
+
+    asText(): string {
+        return this.original.asText();
+    }
+}
---
+++ test/GeometryWithCachedEnvelope.spec.ts
@@ -0,0 +1,40 @@
+import GeometryWithCachedEnvelope from "../src/GeometryWithCachedEnvelope";
+import "mocha";
+import { expect } from "chai";
+import Point from "../src/Point";
+
+
+describe("test GeometryWithCachedEnvelope ", () => {
+
+
+    it("should work for existing methods", () => {
+        let point = new Point([3.0, 3.0]);
+        let g = new GeometryWithCachedEnvelope(point);
+        expect(g.getType()).to.equal("Point");
+        expect(g.isEmpty()).to.equal(false);
+        expect(g.asText()).to.equal("POINT(3 3)");
+        
+        const copy = g.clone();
+        expect(copy).to.not.equal(g);
+        expect(copy.getType()).to.equal("Point");
+        expect(copy.isEmpty()).to.equal(false);
+    });
+
+
+    it("test getEnvelope() caching", () => {
+        let point = new Point([3.0, 3.0]);
+        let g = new GeometryWithCachedEnvelope(point);
+        const a = g.getEnvelope();
+        const b = g.getEnvelope();
+        expect(a).to.equal(b);
+    });
+
+    it("test translate invalidates cache", () => {
+        let point = new Point([3.0, 3.0]);
+        let g = new GeometryWithCachedEnvelope(point);
+        const a = g.getEnvelope();
+        g.translate(1.0, 1.0);
+        const b = g.getEnvelope();
+        expect(a).to.not.equal(b);
+    });
+});
